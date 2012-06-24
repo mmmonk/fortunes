a3a363221ee1bb7a5a8f1a5760fed0635e19a35d
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-06-24 20:37:59 +0200

    Sun Jun 24 20:37:59 CEST 2012

--- finddupquotes.py
+++ finddupquotes.py
@@ -5,46 +5,72 @@ import re
 
 fd = sys.argv[1]
 
+pmatch = 1
+try:
+  pmatch = float(sys.argv[2])
+except:
+  pass
+
 text = open(fd).read()
-qlist = re.findall("(?:^|%)(.+?)%",text,flags=re.S|re.M)
+qlist = re.findall("(?:^|%\n)(.+?)\n%\n",text,flags=re.S|re.M)
 
-i = 0
-
-empty = [ 1 for x in xrange(0,1000) ]
+empty = [ 1 for x in xrange(0,100) ]
 
 quotes = {}
 qcmp = {}
 
+i = 0
 for quote in qlist:
   quotes[i] = quote
-  qcmp[i] = re.sub("(\s|~|,|\.|'|\"|`|!|-)+"," ",quote.lower())
-  qcmp[i] = re.sub(" +"," ",qcmp[i])
+  qcmp[i] = re.sub("(\s|~|,|\.|'|\"|`|!|-|:|;|\)|\()+"," ",quote.lower())
   qcmp[i] = dict(zip(qcmp[i].split(),empty)).keys()
+  qcmp[i].sort(key = lambda x: len(x))
   i += 1
 
 del(qlist)
+del(empty)
 
-for k,q in qcmp.iteritems(): 
-  for k1,q1 in qcmp.iteritems():
-    if k == k1:
-      continue
+qcmp = sorted(qcmp.items(),key = lambda x: len(x[1]))
+qcmp1 = qcmp[:]
+
+for idx,q in qcmp:
+  lq = int(pmatch*len(q))
+
+  try:
+    qcmp1.remove((idx,q))
+  except ValueError:
+    pass
+
+  if len(qcmp1) % 10 == 0 or len(qcmp1) < 200:
+    sys.stderr.write("quotes left to check: "+str(len(qcmp1))+"\n")
+
+  for idxq1,q1 in qcmp1:
+    same = 0
 
     if q == q1:
-      print "###################repeat#####################\n"
-      print quotes[k1]
-      print "##############################################\n"
-      continue
-     
-    if len(q) >= 2*len(q1) or len(q1) > 2*len(q):
-      continue
+      same = 1
 
-    same = 0
-    for i1 in q:
-      for i2 in q1:
-        if i1 == i2:
-          same += 1
+    elif q in q1:
+      same = 1
 
-    if same >= len(q):
-      print "##############################################"
-      print quotes[k]+"\n-=-=-=-=-=-=-=-= the same as =-=-=-=-=-=-=-=-\n"+quotes[k1]
+    else:
+      csame = 0
+
+      for item in q:
+        if csame >= lq:
+          break
+        try:
+          q1.index(item)
+          csame += 1
+        except ValueError:
+          pass
+
+      if csame >= lq:
+        same = 1
+
+    if same == 1:
+      print "###############################################"
+      print quotes[idx]
+      print "-=-=-=-=-=-=-=-= the same as =-=-=-=-=-=-=-=-=-"
+      print quotes[idxq1]
 
